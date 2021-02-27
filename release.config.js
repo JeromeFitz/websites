c6a8138f033de5c18473282f1825e24af2ea17a3
Jerome Fitzgerald <jeromes@gmail.com>
2021-02-27 17:25:59 -0500

    ✨   `@jeromefitz/semantic@2.0.0-develop.3` 😬️ mutate plugins override

--- release.config.js
+++ release.config.js
@@ -1,13 +1,10 @@
-const release = require('@jeromefitz/semantic/release.config.js')
+const plugins = require('@jeromefitz/semantic/release.config.js').plugins
 
 // const ci = true
 // const dryRun = false
 const _extends = ['semantic-release-commit-filter']
 
-// @todo Uh, this pretty jank dude.
-const plugins = [
-  release.plugins[0],
-  release.plugins[1],
+const pluginsOverride = [
   [
     '@semantic-release/npm',
     {
@@ -18,14 +15,17 @@ const plugins = [
     '@semantic-release/github',
     { labels: false, releasedLabels: false, successComment: false },
   ],
-  [
-    '@semantic-release/git',
-    {
-      assets: ['package.json'],
-      message: `🔖️ {PACKAGE_NAME}@{VERSION} [skip ci]\n\n{RELEASE_URL}/releases/tag/{RELEASE_TAG}\n\n{RELEASE_NOTES}`,
-    },
-  ],
 ]
+
+/**
+ * @refactor This mutates plugins which is not ideal
+ */
+plugins.map((plugin, pluginIndex) => {
+  const pluginName = plugin[0]
+  pluginsOverride.map((pluginOverride) => {
+    pluginName === pluginOverride[0] ? (plugins[pluginIndex] = pluginOverride) : null
+  })
+})
 
 module.exports = {
   ...release,
