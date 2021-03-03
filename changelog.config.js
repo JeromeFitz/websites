77ffb04336c82839d602b9d048948455072e5fd4
Jerome Fitzgerald <jeromes@gmail.com>
2021-03-02 22:39:33 -0500

    ✨ add format to changelog for gitmoji way

--- changelog.config.js
+++ changelog.config.js
@@ -1,4 +1,5 @@
 // const changelog = require('@jeromefitz/semantic/src/changelog.config.js')
+const format = '{emoji}{scope} {subject}'
 const scopes = ['', 'release']
 const questions = ['type', 'subject', 'body']
-module.exports = { questions, scopes }
+module.exports = { format, questions, scopes }
