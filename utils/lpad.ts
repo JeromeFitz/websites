18e3a2c834c2120e1b90039d7c68bfe593b9e1ef
Jerome Fitzgerald <jeromes@gmail.com>
2021-03-11 12:23:51 -0500

    ♻️  better lpad (commented out for now)

--- utils/lpad.ts
+++ utils/lpad.ts
@@ -2,6 +2,7 @@
  *
  * @todo Make this dynamic
  */
+// const lpad = (value, length, char) => String(value).padStart(length, char)
 // eslint-disable-next-line @typescript-eslint/restrict-plus-operands
 const lpad = (val: any) => ('00' + val).substr(-2)
 
