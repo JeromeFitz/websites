e52c742118caa551b814d94b6057b967201f34d7
Jerome Fitzgerald <jeromes@gmail.com>
2021-04-04 01:18:38 -0400

    🚨  eslint-parser

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,6 @@
 module.exports = {
   ...require('@jeromefitz/codestyle/.eslintrc.js'),
   // @hack(codestyle) why is this not getting lifted?
-  parser: '@jeromefitz/codestyle/node_modules/@babel/eslint-parser',
+  // parser: '@jeromefitz/codestyle/node_modules/@babel/eslint-parser',
+  parser: '@babel/eslint-parser',
 }
