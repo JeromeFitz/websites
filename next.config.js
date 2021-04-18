5be94c8543a98c14818733e73a38c1759aeaced3
Jerome Fitzgerald <jeromes@gmail.com>
2021-04-18 13:08:41 -0400

    ✨  ContentSecurityPolicy, opt-out of Google FLoC 👿️
    This also updates `dotenv`, `next`, and `webpack`

--- next.config.js
+++ next.config.js
@@ -17,6 +17,57 @@ if (process.env.NEXT_PUBLIC__SITE !== urlBaseCheck) {
   throw new Error(`process.env.NEXT_PUBLIC__SITE is not: ${urlBaseCheck}`)
 }
 
+// https://securityheaders.com
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' *.youtube.com *.twitter.com cdn.usefathom.com;
+  child-src *.youtube.com *.google.com *.twitter.com;
+  style-src 'self' 'unsafe-inline' *.googleapis.com;
+  img-src * blob: data:;
+  media-src 'none';
+  connect-src *;
+  font-src 'self';
+`
+
+const securityHeaders = [
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP
+  {
+    key: 'Content-Security-Policy',
+    value: ContentSecurityPolicy.replace(/\n/g, ''),
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Referrer-Policy
+  {
+    key: 'Referrer-Policy',
+    value: 'origin-when-cross-origin',
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Frame-Options
+  {
+    key: 'X-Frame-Options',
+    value: 'DENY',
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Content-Type-Options
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff',
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-DNS-Prefetch-Control
+  {
+    key: 'X-DNS-Prefetch-Control',
+    value: 'on',
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Strict-Transport-Security
+  {
+    key: 'Strict-Transport-Security',
+    value: 'max-age=31536000; includeSubDomains; preload',
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Feature-Policy
+  // Opt-out of Google FLoC: https://amifloced.org/
+  {
+    key: 'Permissions-Policy',
+    value: 'camera=(), microphone=(), geolocation=(), interest-cohort=()',
+  },
+]
+
 const nextConfig = {
   amp: false,
   assetPrefix: '',
@@ -32,7 +83,15 @@ const nextConfig = {
     polyfillsOptimization: true,
     productionBrowserSourceMaps: false,
   },
-  future: { webpack5: true },
+  future: { strictPostcssConfiguration: true, webpack5: true },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: securityHeaders,
+      },
+    ]
+  },
   images: {
     domains: [
       'cdn.jerandky.com', // CDN
