3b594edd8c918f654519778f403fa55115689847
Jerome Fitzgerald <jeromes@gmail.com>
2021-06-16 01:18:06 -0400

    🩹  fix release.config off of new semantic

--- release.config.js
+++ release.config.js
@@ -2,7 +2,11 @@ const release = require('@jeromefitz/semantic/release.config.js')
 const isCI = require('is-ci')
 !isCI && require('dotenv').config({ path: './.env.build' })
 
+const { name } = require('./package.json')
+
 const plugins = release.plugins
+
+const branches = [{ name: 'main' }, { name: 'develop', prerelease: 'develop' }]
 
 // const ci = true
 // const dryRun = false
@@ -33,8 +37,11 @@ plugins.map((plugin, pluginIndex) => {
 
 module.exports = {
   ...release,
+  branches,
   // ci,
   // dryRun,
   extends: _extends,
   plugins,
+  repositoryUrl: `https://github.com/${name.replace('@', '')}`,
+  tagFormat: 'website-v${version}',
 }
