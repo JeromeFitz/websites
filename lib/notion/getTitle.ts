eb13128d86566201e80d4507cb142f7585034471
Jerome Fitzgerald <j@jeromefitzgerald.com>
2021-10-22 14:13:54 -0400

    🚧  rollups: cannot do anyting but titles just yet reduce api calls people

--- lib/notion/getTitle.ts
+++ lib/notion/getTitle.ts
@@ -1,6 +1,7 @@
 const getTitle = (title) =>
   title
     .replace('people', '')
+    .replace('rollup', '')
     .replace('DirectorMusical', 'Musical Director')
     .replace('DirectorTechnical', 'Technical Director')
     .replace('eventsLineupShowIds', 'Lineup')
