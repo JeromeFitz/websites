3b9135cc68626a1435d6f3b4d4d0b39e1d79fac2
Jerome Fitzgerald <j@jeromefitzgerald.com>
2021-10-27 10:04:31 -0400

    ⬆️  plaiceholder@2.2.0 w/ next@12 support 🔥️ patch

--- next.config.js
+++ next.config.js
@@ -3,10 +3,9 @@ require('dotenv').config({ path: './.env.build' })
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
-// const { withPlaiceholder } = require('@plaiceholder/next')
+const { withPlaiceholder } = require('@plaiceholder/next')
 const { withPlugins } = require('next-compose-plugins')
 
-const { withPlaiceholder } = require('./patches/@plaiceholder/next/dist')
 // const withPWA = require('next-pwa')
 
 // const getRedirects = require('./config/notion/website/getRedirects')
