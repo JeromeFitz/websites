d639701e73043d9d78837f081c8c8abfd58a5bed
Jerome Fitzgerald <j@jeromefitzgerald.com>
2021-11-13 14:01:53 -0500

    💄  mobile for get tickets

--- styles/system/components/Button/MarketingButton.tsx
+++ styles/system/components/Button/MarketingButton.tsx
@@ -92,7 +92,7 @@ export const MarketingButton = React.forwardRef<
     <StyledButton ref={forwardedRef} {...props}>
       {children}
       {Icon && (
-        <Box as="span" css={{ ml: 8, mr: -3 }}>
+        <Box as="span" css={{ ml: 8, mr: -3, minWidth: '1rem', minHeight: '1rem' }}>
           <Icon />
         </Box>
       )}
