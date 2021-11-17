16048c4a01075e054e94c5312adc0f262c53d0e5
Jerome Fitzgerald <j@jeromefitzgerald.com>
2021-11-17 10:25:54 -0500

    ♻️  CastGuest w/ MusicGuest (ajv)

--- lib/notion/getTitle.ts
+++ lib/notion/getTitle.ts
@@ -2,10 +2,11 @@ const getTitle = (title) =>
   title
     .replace('people', '')
     .replace('rollup', '')
-    .replace('CastGuest', 'Special Guest')
+    .replace('CastGuest', 'Guest Performer')
     .replace('CastPast', 'Cast Emeritus')
     .replace('DirectorMusical', 'Musical Director')
     .replace('DirectorTechnical', 'Technical Director')
+    .replace('MusicGuest', 'Musical Guest')
     .replace('eventsLineupShowIds', 'Lineup')
     .replace('tags', 'Tag')
 
