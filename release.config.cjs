289aeccfbc190b790929e86f5502ef91e47f5462
Jerome Fitzgerald <j@jeromefitzgerald.com>
2021-11-26 17:15:46 -0500

    💥  @jeromefitz/codestyle|git-cz|semantic esm breaking [build]
    💥️  BREAKING CHANGE: packages via esm

--- release.config.cjs
+++ release.config.cjs
@@ -2,11 +2,30 @@ const release = require('@jeromefitz/semantic/release.config.js')
 const isCI = require('is-ci')
 !isCI && require('dotenv').config({ path: './.env.build' })
 
+const releaseBranchTypes = require('./config/release-branch-types/index.cjs')
 const { name } = require('./package.json')
 
 const plugins = release.plugins
 
-const branches = [{ name: 'main' }, { name: 'develop', prerelease: 'develop' }]
+const branchTypes = _map(
+  releaseBranchTypes,
+  (releaseBranchType, releaseBranchTypeIndex) => {
+    return _map(releaseBranchType, (branchType) => {
+      return (
+        !!branchType && {
+          name: `${releaseBranchTypeIndex}/${branchType}`,
+          prerelease: branchType,
+        }
+      )
+    })[0]
+  }
+).filter((branchType) => !!branchType)
+
+const branches = [
+  { name: 'main' },
+  { name: 'canary', prerelease: 'canary' },
+  ...branchTypes,
+]
 
 // const ci = true
 // const dryRun = false
