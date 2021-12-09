ec361112878769cc0d67acf8458ff2a3317eae58
Jerome Fitzgerald <j@jeromefitzgerald.com>
2021-12-09 10:21:08 -0500

    ⬆️  eslint8, @jeromefitz, radix-ui@0.1.2 🔥️ DesignSystemProvider (#711)

--- styles/system/components/DesignSystemProvider.tsx
+++
@@ -1,6 +0,0 @@
-import { IdProvider } from '@radix-ui/react-id'
-import React from 'react'
-
-export const DesignSystemProvider: React.FC = ({ children }) => (
-  <IdProvider>{children}</IdProvider>
-)
