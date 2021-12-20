476d13c07efcd44d5a5d3ef593986f3138a93713
Jerome Fitzgerald <j@jeromefitzgerald.com>
2021-12-19 19:14:21 -0500

    🔥  backgrounds + carousel

--- src/components/Music/TopArtists.tsx
+++ src/components/Music/TopArtists.tsx
@@ -27,6 +27,8 @@ import { Breakout } from '~components/Container'
 import useSpotify from '~hooks/useSpotify'
 import fetcher from '~lib/fetcher'
 
+import { backgrounds } from './index.props'
+
 const HOUR = 3600000
 // const MINUTE = 60000
 // const SECOND = 1000
@@ -46,48 +48,6 @@ const css_icon = {
   width: '1rem',
 }
 
-const backgrounds = [
-  {
-    light: 'linear-gradient(120deg, $indigo6, $crimson5)',
-    dark: 'linear-gradient(120deg, $indigo4, $plum3)',
-  },
-  {
-    light: 'linear-gradient(120deg, $crimson5, $blue5)',
-    dark: 'linear-gradient(120deg, $plum3, $blue3)',
-  },
-  {
-    light: 'linear-gradient(120deg, $blue5, $lime3)',
-    dark: 'linear-gradient(120deg, $blue3, $sand6)',
-  },
-  {
-    light: 'linear-gradient(120deg, $lime3, $pink4)',
-    dark: 'linear-gradient(120deg, $sand6, $pink3)',
-  },
-  {
-    light: 'linear-gradient(120deg, $pink4, $gold5)',
-    dark: 'linear-gradient(120deg, $pink3, $gold4)',
-  },
-  {
-    light: 'linear-gradient(120deg, $gold5, $tomato5)',
-    dark: 'linear-gradient(120deg, $gold4, $crimson4)',
-  },
-  {
-    light: 'linear-gradient(120deg, $tomato5, $indigo7)',
-    dark: 'linear-gradient(120deg, $crimson4, $indigo5)',
-  },
-  {
-    light: 'linear-gradient(120deg, $indigo7, $cyan3)',
-    dark: 'linear-gradient(120deg, $indigo5, $cyan7)',
-  },
-  {
-    light: 'linear-gradient(120deg, $cyan3, $mint5)',
-    dark: 'linear-gradient(120deg, $cyan7, $teal6)',
-  },
-  {
-    light: 'linear-gradient(120deg, $mint5, $red3)',
-    dark: 'linear-gradient(120deg, $teal6, $plum4)',
-  },
-]
 const backgroundsSize = _size(backgrounds)
 const info = {
   error: {
