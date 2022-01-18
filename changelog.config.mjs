a28e9ec144a48cdd3d4685f72e60018cfa896bd8
Jerome Fitzgerald <j@jeromefitzgerald.com>
2022-01-18 17:55:23 -0500

    👷  changelog, lint-staged updates

--- changelog.config.mjs
+++ changelog.config.mjs
@@ -1,20 +1,41 @@
-import config, { commit as _commit } from '@jeromefitz/semantic/changelog.config.js'
+import isCI from 'is-ci'
 
-const changelog = {
-  ...config,
-  commit: {
-    ..._commit,
-    questions: [
-      'branchFlag',
-      'commitBreakingFlag',
-      'commitBreaking',
-      // 'commitScopes',
-      'commitTypes',
-      'commitSubject',
-      'commitBodyFlag',
-      'commitBody',
-    ],
-  },
+if (!isCI) {
+  const dotenv = await import('dotenv')
+  dotenv.config({ path: './.env' })
 }
+
+const isOverride = process.env.GIT_CZ__OVERRIDE_TEST || false
+
+const enabled = isOverride
+
+const _types = {}
+
+const commit = isOverride
+  ? {
+      after: { branchName: ' ', emoji: '  ', scope: ') ' },
+      before: { branchName: '', emoji: '', scope: '(' },
+      format: '{emoji}{scope}{branchName}{subject}',
+      maxMessageLength: 64,
+      minMessageLength: 3,
+      questions: [
+        'branchFlag',
+        'commitBreakingFlag',
+        'commitBreaking',
+        // 'commitScopes',
+        'commitTypes',
+        'commitSubject',
+        'commitBodyFlag',
+        'commitBody',
+      ],
+      scopes: [],
+    }
+  : {}
+
+const branch = {}
+
+const types = isOverride ? _types : {}
+
+const changelog = { branch, commit, enabled, types }
 
 export default changelog
