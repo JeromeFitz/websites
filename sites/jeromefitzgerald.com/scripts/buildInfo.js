f2a6a9af82fe4c38ab5545a8b2826ac6f25918d6
Jerome Fitzgerald <j@jeromefitzgerald.com>
2022-01-27 17:02:33 -0500

    🩹️ (turbo@1.1.1) optimizations 🚚️ ./sites 👷️ `buildInfo.json` 🔺️ (#833)
    - 🍱️ imagery updates that require a new vercel build
    - 👷 buildInfo.json hack for next.config.js (rewrites)
    - ⬆️ turbo@1.1.1, @notionhq/client@0.4.13, swr@1.2.0, ... 
    - 👷 (turbo) optimizations
    - 🚚 packages => sites (turbo@1.1.1 patch)
    - 🩹 prerelease test
    - 🩹 prerelease test ii
    - 🐛 homepage, do not ssr the ShowsListing [b]

---
+++ sites/jeromefitzgerald.com/scripts/buildInfo.js
@@ -0,0 +1,92 @@
+const childProcess = require('child_process')
+const { writeFile } = require('fs/promises')
+const { join } = require('path')
+
+const { Octokit } = require('@octokit/core')
+const _filter = require('lodash/filter')
+const _orderBy = require('lodash/orderBy')
+const _pick = require('lodash/pick')
+const _size = require('lodash/size')
+const prettier = require('prettier')
+
+const octokit = new Octokit({ auth: process.env.GH_TOKEN })
+
+const branch =
+  process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF ||
+  childProcess
+    // @note(git>=2.22)
+    // .execSync(`git branch --show-current`)
+    .execSync(`git rev-parse --abbrev-ref HEAD`)
+    .toString()
+    .trim()
+
+function getBranch(branch) {
+  if (_size(branch.split('/')) > 1) {
+    return branch.split('/')[1]
+  }
+
+  return branch
+}
+
+async function setupBuildInfo() {
+  const releases = await octokit.request('GET /repos/{owner}/{repo}/releases', {
+    owner: 'jeromefitz',
+    repo: 'jeromefitzgerald.com',
+    page: 1,
+    per_page: 20,
+  })
+
+  const ___data = _orderBy(releases.data, ['published_at'], ['desc'])
+
+  const dataBranch = _filter(___data, {
+    target_commitish: branch,
+  })
+  const dataRelease = _filter(___data, { prerelease: false })
+
+  const __data = _size(dataBranch) > 0 ? dataBranch[0] : dataRelease[0]
+
+  const _data = _pick(__data, [
+    'body',
+    'tag_name',
+    'target_commitish',
+    'name',
+    'prerelease',
+    'published_at',
+  ])
+
+  const [version, prerelease] = _data.tag_name.replace('website-v', '').split('-')
+  const [major, minor, patch] = version.split('.')
+
+  const data = {
+    branch: getBranch(branch),
+    branchFull: branch,
+    isBranchMain: branch === 'main',
+    major,
+    minor,
+    patch,
+    prerelease: !!prerelease ? prerelease : getBranch(branch),
+    version,
+  }
+
+  const filePath = join(process.cwd(), './src/config/buildInfo.json')
+  const content = prettier.format(JSON.stringify(data), { parser: 'json' })
+  await writeFile(filePath, content)
+}
+
+/**
+ *
+ * @hack next.config
+ *
+ * hi-jack `rewrites` to use `async` during build process.
+ */
+function withBuildInfo(nextConfig = {}) {
+  const { rewrites } = nextConfig
+  nextConfig.rewrites = async (...args) => {
+    await setupBuildInfo()
+    return rewrites?.(...args) ?? {}
+  }
+
+  return nextConfig
+}
+
+module.exports = { withBuildInfo }
---
+++ sites/jeromefitzgerald.com/src/components/Layout/ImageLead.tsx
@@ -0,0 +1,122 @@
+import {
+  Breakout,
+  Caption,
+  Container,
+  Section,
+  Skeleton,
+} from '@jeromefitz/design-system/components'
+import _isEmpty from 'lodash/isEmpty'
+import useSWR from 'swr'
+
+import fetcher from '~lib/fetcher'
+
+import { Image, ImageBlur, ImageContainer } from './ImageLead.styles'
+
+/**
+ * @optimization
+ *
+ * Just because we know the WIDTH/HEIGHT of images, does not mean
+ *  we shoudl be passing them to `Image` (next/image`)
+ *
+ *
+ */
+const ImageWithBackgroundBlur = ({
+  base64,
+  description,
+  image,
+  priority = false,
+  // sizes = '(max-width: 1280) 10vh, 30vh',
+  // // sizes = '256px',
+  slug,
+}) => {
+  const { height, src, width } = image
+
+  return (
+    <ImageContainer>
+      <ImageBlur
+        css={{
+          backgroundImage: `url(${base64})`,
+          backgroundSize: 'cover',
+          borderRadius: '$4',
+        }}
+      />
+
+      <Image
+        alt={description}
+        blurDataURL={base64}
+        layout="intrinsic"
+        key={slug}
+        placeholder="blur"
+        priority={priority}
+        quality={90}
+        src={src}
+        height={height}
+        width={width}
+      />
+    </ImageContainer>
+  )
+}
+
+const ImageSkeleton = () => {
+  return (
+    <ImageContainer css={{ height: '250px', width: '250px' }}>
+      <Skeleton
+        as="div"
+        css={{
+          height: '100%',
+          width: '100%',
+        }}
+      >
+        &nbsp;
+      </Skeleton>
+    </ImageContainer>
+  )
+}
+
+const ImageLead = ({ breakout = true, description, image, images }) => {
+  /**
+   * @refactor(images) passing images from SSR is not ideal
+   * @note(notion) this is based off of seoImage only at the moment
+   * @note(image) check against the first key in `images` only (seoImage)
+   */
+  const imageSlug = !!image && Object.keys(image)[0]
+  const url = !!imageSlug && image[imageSlug]?.url
+  const fallbackData = !!url && !!images ? images[imageSlug] : {}
+  // @note(image) do not call if we do not need to
+  const urlApi = !!url && _isEmpty(fallbackData) ? `/api/images?url=${url}` : null
+
+  const { data } = useSWR<any>(urlApi, fetcher, {
+    fallbackData,
+  })
+
+  // @note(image) verify it has been optimized
+  // @todo(image) fallback base64
+  const hasImage = !!data && !!data?.base64
+
+  if (!hasImage) {
+    return null
+  }
+
+  // @todo(types)
+  const WrapComponent: any = breakout ? Breakout : Section
+
+  return (
+    <>
+      <WrapComponent>
+        <Container size="2">
+          <ImageWithBackgroundBlur
+            base64={data?.base64}
+            description={description}
+            image={data?.img}
+            priority={true}
+            slug={imageSlug}
+          />
+          <Caption>{description}</Caption>
+        </Container>
+      </WrapComponent>
+    </>
+  )
+}
+
+export { ImageSkeleton, ImageWithBackgroundBlur }
+export default ImageLead
