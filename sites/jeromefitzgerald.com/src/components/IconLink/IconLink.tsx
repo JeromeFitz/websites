64590b261af773e5823e22a5bf6246a09fea42f4
Jerome Fitzgerald <j@jeromefitzgerald.com>
2022-02-23 18:54:40 -0500

    🚨  IconLink passHref [skip ci]

--- sites/jeromefitzgerald.com/src/components/IconLink/IconLink.tsx
+++ sites/jeromefitzgerald.com/src/components/IconLink/IconLink.tsx
@@ -25,7 +25,7 @@ const LinkInternal = (props: any) => {
   const link = getNextLink(href)
   const text = getText(children)
   return (
-    <NextLink as={link.as} href={link.href}>
+    <NextLink as={link.as} href={link.href} passHref>
       <Link type="icon" {...props}>
         {text}
       </Link>
