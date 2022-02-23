b20f6104a88f88fb0eae9bc4783fcbd77a72e292
Jerome Fitzgerald <j@jeromefitzgerald.com>
2022-02-23 09:07:22 -0500

    ➕  contribs prerelease test

--- release.config.cjs
+++ release.config.cjs
@@ -4,11 +4,6 @@ const _map = require('lodash/map.js')
 
 !isCI && require('dotenv').config({ path: './.env' })
 
-const releaseBranchTypes = require('./config/release-branch-types/index.cjs')
-const { name } = require('./package.json')
-
-const plugins = release.plugins
-
 const branchTypes = _map(
   releaseBranchTypes,
   (releaseBranchType, releaseBranchTypeIndex) => {
@@ -29,48 +24,13 @@ const branches = [
   ...branchTypes,
 ]
 
-// const ci = true
-// const dryRun = false
-const _extends = ['semantic-release-commit-filter']
-
-const pluginsOverride = [
-  [
-    '@semantic-release/npm',
-    {
-      npmPublish: false,
-    },
-  ],
-  [
-    '@semantic-release/github',
-    {
-      addReleases: false,
-      labels: false,
-      releasedLabels: false,
-      successComment: false,
-    },
-  ],
-]
-
-/**
- * @refactor This mutates plugins which is not ideal
- */
-plugins.map((plugin, pluginIndex) => {
-  const pluginName = plugin[0]
-  pluginsOverride.map((pluginOverride) => {
-    pluginName === pluginOverride[0] ? (plugins[pluginIndex] = pluginOverride) : null
-  })
-})
-
-const releaseConfig = {
-  ...release,
+const configPassed = {
+  ...configDefault,
   branches,
-  // ci,
-  // dryRun,
-  extends: _extends,
-  plugins,
-  // repositoryUrl: `https://github.com/${name.replace('@', '')}`,
   repositoryUrl: `https://github.com/JeromeFitz/websites`,
   tagFormat: 'website-v${version}',
 }
 
-module.exports = { releaseConfig }
+const config = getConfig(configPassed)
+
+module.exports = config
