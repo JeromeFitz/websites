6c12914dfa679ec6ba36f98bbf16eee398e77b94
Jerome Fitzgerald <j@jeromefitzgerald.com>
2022-05-10 23:53:54 -0400

    🔀 radix-ui w/ react 18 📦 new codestyle packages [b] (#1077)
    - ⬆️ eslint-config|lint-staged|prettier-config|tsconfig 🐦
    - ⬆️ design-system@canary.2; more package upgrades
      - `@mantine/hooks@4.2.4, dotenv@16.0.1, husky@8.0.1, ts-jest@28.0.2` 
    - 📝 README update for `@jeromefitz` 📦

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,4 @@
-const config = require('@jeromefitz/codestyle/.eslintrc.next.cjs')
-
 module.exports = {
-  ...config,
-  // parser: './node_modules/@jeromefitz/node_modules/@babel/eslint-parser/lib',
+  extends: '@jeromefitz/eslint-config/next.cjs',
+  root: true,
 }
