3b89b753fdf186446671fc8826cffdc179b8b92f
Jerome Fitzgerald <j@jeromefitzgerald.com>
2022-06-24 09:25:35 -0400

    👷 ⬆️ (deps) github actions :octocat: seo pronoun + [skip ci] (#1160)

--- sites/jeromefitzgerald.com/src/config/seo.ts
+++ sites/jeromefitzgerald.com/src/config/seo.ts
@@ -7,7 +7,7 @@ const seo = {
   sitename: 'Jerome Fitzgerald',
   title: 'Jerome Fitzgerald | Comedian. Human. Nice. ',
   description:
-    'Jerome Fitzgerald is an actor, comedian, & writer hailing from Pittsburgh, PA.',
+    'Jerome Fitzgerald (he/him) is an actor, comedian, & writer hailing from Pittsburgh, PA.',
   url: 'https://jeromefitzgerald.com',
   twitter: '@JeromeFitz',
 }
