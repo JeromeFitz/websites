8e386135e1f37ca4dfcec23c7e8647ebccfb8300
Jerome Fitzgerald <j@jeromefitzgerald.com>
2023-04-30 22:16:45 -0400

    🚑️  (analytics) fix errant fathom check

--- sites/jeromefitzgerald.com/src/components/Analytics/Fathom.tsx
+++ sites/jeromefitzgerald.com/src/components/Analytics/Fathom.tsx
@@ -4,15 +4,16 @@ import { load, trackPageview } from 'fathom-client'
 import { usePathname, useSearchParams } from 'next/navigation'
 import { useEffect } from 'react'
 
-const isProd = process.env.NODE_ENV === 'production'
-const isVercel = process.env.VERCEL_URL || ''
-const hasAnalytics = isProd && isVercel
+// const isProd = process.env.NODE_ENV === 'production'
+// const isVercel = process.env.VERCEL_URL || ''
+// const hasAnalytics = isProd && isVercel
 
 function FathomAnalytics() {
   const pathname = usePathname()
   const searchParams = useSearchParams()
   useEffect(() => {
-    if (hasAnalytics) {
+    // if (hasAnalytics) {
+    if (process.env.NODE_ENV === 'production') {
       /**
        * @note(types)
        * Argument of type 'string | undefined' is not assignable to parameter of type 'string'.
