8823752052b1368773ce816cd6f1d75322534664
Jerome Fitzgerald <j@jeromefitzgerald.com>
2023-08-28 22:58:14 -0400

    ⬆️  (next) next@13.4.20-canary.11, 🐛 PreloadResources [b] (#1835)
    - ⬆️  (deps) next@13.4.20-canary.11
      - Hack for Preload Resources `next/head` is 👻
    - 🩹 override cache hack 
      - I weep that this happens from time to time with listings and the circular logic
      - Please fix in next 30 days
    
    This PR (still) does not pull from latest DB to make this dynamic
    - And need to run a build with `OVERRIDE_CACHE` to re-fill DB properly

---
+++ sites/jeromefitzgerald.com/src/app/_next/preload-resources.tsx
@@ -0,0 +1,27 @@
+'use client'
+
+import ReactDOM from 'react-dom'
+
+const preconnects = [
+  // https://web.dev/preconnect-and-dns-prefetch/#how-to-implement-rel=preconnect
+  // 'https://jeromefitzgerald.com',
+  'https://cdn.jeromefitzgerald.com',
+  'https://cdn.usefathom.com',
+  'https://vitals.vercel-insights.com',
+]
+
+function PreloadResources() {
+  preconnects.map((preconnect) => {
+    // console.dir(`> preconnect: ${preconnect}`)
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    ReactDOM.preconnect(preconnect, { crossOrigin: 'anonymous' })
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    ReactDOM.prefetchDNS(preconnect)
+  })
+
+  return null
+}
+
+export { PreloadResources }
