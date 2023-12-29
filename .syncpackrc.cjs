947c3e3a2a340f86b5121dcb5e539fa9642fd329
Jerome Fitzgerald <j@jeromefitzgerald.com>
2023-12-29 11:02:21 -0500

    ♻️ (syncpack) update for 12; prep for fixes
    Need to look at these now:
    
    ```bash
    Versions
    = workspace protocol when local ================================================
        25 ✓ already valid
    = Default Version Group ========================================================
    ✘ @mantine/hooks ^6.0.8 || ^7.0.0 → ^7.3.2 packages/design-system/package.json > peerDependencies [HighestSemverMismatch]
    ✘ @notionhq/client ^2.2.6 → ^2.2.14 packages/next-notion/package.json > peerDependencies [HighestSemverMismatch]
    ✘ @notionhq/client ^2.2.6 → ^2.2.14 packages/shared/package.json > peerDependencies [HighestSemverMismatch]
    ✘ @upstash/ratelimit ^0.4.3 || ^1.0.0 → ^1.0.0 packages/shared/package.json > peerDependencies [HighestSemverMismatch]
    ✘ @upstash/redis ^1.22.0 → ^1.27.1 packages/shared/package.json > peerDependencies [HighestSemverMismatch]
    ✘ @vercel/analytics ^1.0.1 → ^1.1.1 packages/shared/package.json > peerDependencies [HighestSemverMismatch]
    ✘ fathom-client ^3.5.0 → ^3.6.0 packages/shared/package.json > peerDependencies [HighestSemverMismatch]
    ✘ react-tweet ^3.0.3 → ^3.2.0 packages/shared/package.json > peerDependencies [HighestSemverMismatch]
    ✘ tailwind-merge ^1.13.2 || ^2.0.0 → ^2.2.0 packages/shared/package.json > peerDependencies [HighestSemverMismatch]
    ✘ uuid ^9.0.0 → ^9.0.1 packages/next-notion/package.json > peerDependencies [HighestSemverMismatch]
       422 ✓ already valid
        10 ✓ can be auto-fixed
    Semver Ranges
    = the version property of package.json files must always be exact ==============
    ✓ 13 valid
    = ignore: semver ===============================================================
         3 - 3 ignored
    = types:  !peer ================================================================
    ✓ 412 valid
    = types:  peer =================================================================
    ✓ 25 valid
    ```

---
+++ .syncpackrc.cjs
@@ -0,0 +1,48 @@
+// @ts-check
+
+/** @type {import("syncpack").RcFile} */
+const config = {
+  dependencyTypes: [
+    'dev', // devDependencies
+    'local', // version
+    'overrides', // overrides
+    'peer', // peerDependencies
+    'pnpmOverrides', // pnpm.overrides
+    'prod', // dependencies
+    'resolutions', // resolutions
+  ],
+  indent: '  ',
+  semverGroups: [
+    {
+      dependencies: ['semver*'],
+      dependencyTypes: ['pnpmOverrides', 'resolutions'],
+      isIgnored: true,
+      label: 'ignore: semver',
+      packages: ['**'],
+    },
+    {
+      dependencies: ['**'],
+      dependencyTypes: ['dev', 'local', 'overrides', 'prod'],
+      label: 'types:  !peer',
+      packages: ['**'],
+      range: '',
+    },
+    {
+      dependencies: ['**'],
+      dependencyTypes: ['peer'],
+      label: 'types:  peer',
+      packages: ['**'],
+      range: '^',
+    },
+  ],
+  versionGroups: [
+    {
+      dependencies: ['$LOCAL'],
+      dependencyTypes: ['dev', 'prod'],
+      label: 'workspace protocol when local',
+      pinVersion: 'workspace:*',
+    },
+  ],
+}
+
+module.exports = config
