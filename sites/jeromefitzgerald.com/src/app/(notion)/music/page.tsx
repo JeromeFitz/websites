f8e67ac54f144179a64d36a6cab7302e82cdfb45
Jerome Fitzgerald <j@jeromefitzgerald.com>
2024-02-22 12:07:14 -0500

    🔍️  (seo) fix music 🚚 Top to Music.client [b]

--- sites/jeromefitzgerald.com/src/app/(notion)/music/page.tsx
+++ sites/jeromefitzgerald.com/src/app/(notion)/music/page.tsx
@@ -1,29 +1,70 @@
-import { cx } from '@jeromefitz/ds/utils/cx'
+import { getDataFromCache, getSegmentInfo } from '@jeromefitz/shared/notion/utils'
+import { isObjectEmpty } from '@jeromefitz/utils'
 
-import { Top } from '~app/playground/2024/_components/Top'
+import type { Metadata } from 'next'
 
-function Section() {
-  return <div className={cx()}></div>
+import { draftMode } from 'next/headers'
+
+import { CONFIG, getPageData } from '~app/(notion)/_config'
+import { generateMetadataCustom } from '~app/(notion)/_config/temp/generateMetadataCustom'
+
+import { MusicClient } from './_components/Music.client'
+
+const slug = '/music'
+const { SEGMENT } = CONFIG.MUSIC
+
+export async function generateMetadata({ ...props }): Promise<Metadata> {
+  const { isEnabled } = draftMode()
+  const segmentInfo = getSegmentInfo({ SEGMENT, ...props })
+  const data = await getDataFromCache({
+    database_id: '',
+    draft: isEnabled,
+    filterType: 'equals',
+    // @todo(next) revalidate
+    revalidate: false,
+    segmentInfo: {
+      ...segmentInfo,
+      slug,
+    },
+  })
+
+  const is404 = isObjectEmpty(data?.blocks || {})
+  const is404Seo = {
+    title: `404 | ${segmentInfo?.segment} | ${process.env.NEXT_PUBLIC__SITE}`,
+  }
+
+  if (is404) return is404Seo
+
+  const pageData = getPageData(data?.page?.properties) || ''
+  const seo = await generateMetadataCustom({ data, pageData, segmentInfo })
+
+  return pageData?.isPublished ? seo : is404Seo
 }
 
-function SectionContainer() {
-  return (
-    <>
-      <Top />
-      <Section />
-    </>
-  )
+async function Slug({ revalidate, segmentInfo }) {
+  const { isEnabled } = draftMode()
+
+  const data = await getDataFromCache({
+    database_id: '',
+    draft: isEnabled,
+    filterType: 'equals',
+    revalidate,
+    segmentInfo: {
+      ...segmentInfo,
+      slug,
+    },
+  })
+
+  if (isObjectEmpty(data.page)) return null
+  return <MusicClient />
 }
 
-function PageFooter() {
-  return <div className={cx()}></div>
-}
+export default function Page(props) {
+  const revalidate = props?.revalidate || false
+  const segmentInfo = getSegmentInfo({ SEGMENT, ...props })
 
-export default function Page() {
-  return (
-    <>
-      <SectionContainer />
-      <PageFooter />
-    </>
-  )
+  // if (segmentInfo.isIndex) {
+  //   return <Listing srevalidate={revalidate} egmentInfo={segmentInfo} />
+  // }
+  return <Slug revalidate={revalidate} segmentInfo={segmentInfo} />
 }
