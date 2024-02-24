e0579345078fb6a7a96443771078432d0f7bffb9
Jerome Fitzgerald <j@jeromefitzgerald.com>
2024-02-24 13:31:22 -0500

    💩  NICE-71 tailwind breaks with footer client

--- sites/jeromefitzgerald.com/src/app/_temp/Footer.Cmdk.client.tsx
+++ sites/jeromefitzgerald.com/src/app/_temp/Footer.Cmdk.client.tsx
@@ -1,38 +1,43 @@
-'use client'
-import { Tooltip } from '@jeromefitz/ds/components/Tooltip/index'
-
+/**
+ * @note(next) 🔥 🔥 🔥 🔥 🔥
+ *
+ * for some reason if any thing is `use client`
+ * in footer it breaks tailwind
+ *
+ * GOOD TIMES figuring this one out
+ *
+ */
+// 'use client'
 import { Button, Kbd } from '@radix-ui/themes'
 
-import { useStore as _useStore } from '@/store/index'
+// import { useStore as _useStore } from '@/store/index'
 
-const useStore = () => {
-  return _useStore((store) => ({
-    isCmdkOpen: store.isCmdkOpen,
-    isCmdkOpenSet: store.isCmdkOpenSet,
-  }))
-}
+// const useStore = () => {
+//   return _useStore((store) => ({
+//     isCmdkOpen: store.isCmdkOpen,
+//     isCmdkOpenSet: store.isCmdkOpenSet,
+//   }))
+// }
 
 function FooterCmdkClient() {
-  const { isCmdkOpenSet } = useStore()
+  // const { isCmdkOpenSet } = useStore()
   return (
-    <Tooltip description={'Open via Cmd ⌘ + K'}>
-      <Button
-        className="gap-[0.5rem] group-hover:cursor-pointer lg:flex"
-        highContrast
-        //
-        onClick={() => {
-          isCmdkOpenSet()
-        }}
-        radius="medium"
-        size="3"
-        variant="ghost"
-      >
-        <span className="gap-[0.25rem] lg:flex">
-          <Kbd className="font-mono group-hover:cursor-pointer">⌘ + K</Kbd>
-        </span>
-        <p className="text-[var(--mauve-12)]">Command Menu</p>
-      </Button>
-    </Tooltip>
+    <Button
+      // className="gap-[0.5rem] group-hover:cursor-pointer lg:flex"
+      className="gap-[0.5rem] group-hover:bg-transparent lg:flex"
+      highContrast
+      // onClick={() => {
+      //   isCmdkOpenSet()
+      // }}
+      radius="medium"
+      size="3"
+      variant="ghost"
+    >
+      <span className="gap-[0.25rem] lg:flex">
+        <Kbd className="font-mono">⌘ + K</Kbd>
+      </span>
+      <p className="text-[var(--mauve-12)]">Command Menu</p>
+    </Button>
   )
 }
 
