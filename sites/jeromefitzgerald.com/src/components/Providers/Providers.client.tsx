4ff8ebb6d7e911d9385734fcfc8398b6c069b534
Jerome Fitzgerald <j@jeromefitzgerald.com>
2024-02-24 12:41:14 -0500

    💡  (next) NICE-71 do NOT make Cmdk dynamic

--- sites/jeromefitzgerald.com/src/components/Providers/Providers.client.tsx
+++ sites/jeromefitzgerald.com/src/components/Providers/Providers.client.tsx
@@ -1,4 +1,17 @@
 'use client'
+/**
+ * @note(next) ⛔ ⛔ ⛔
+ *
+ * do not, let me repeat, do not
+ * make Cmdk a `next/dynamic` loaded
+ * component
+ *
+ * *.module.css imports break tailwind
+ *  on development if you do and it was
+ *  very, very annoying to figure out
+ *
+ * ⛔ ⛔ ⛔ ⛔ ⛔ ⛔ ⛔ ⛔
+ */
 import { Cmdk } from '@/components/Cmdk'
 import { Orientation } from '@/components/WIP/index'
 
