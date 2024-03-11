d6b5a35330e67eb23d9626a9313cf5ed8aae4104
Jerome Fitzgerald <j@jeromefitzgerald.com>
2024-03-11 16:53:06 -0400

    🚚 (storybook) NICE-93 jsx(x)=>ts(x)

--- packages/storybook-config/withTailwindTheme.decorator.tsx
+++ packages/storybook-config/withTailwindTheme.decorator.tsx
@@ -1,25 +1,23 @@
-import { cx } from '../design-system/src/utils/cx'
-
 import { Box } from '@radix-ui/themes/dist/esm/components/box.js'
 import { Theme } from '@radix-ui/themes/dist/esm/components/theme.js'
 import React, { useEffect } from 'react'
+
+import { cx } from '../design-system/src/utils/cx'
 
 export const DEFAULT_THEME = 'light'
 
 export const withTailwindTheme = (Story, context) => {
   const { theme } = context.globals
 
+  // eslint-disable-next-line react-hooks/rules-of-hooks
   useEffect(() => {
     const htmlTag = document.documentElement
-    const bodyTag = document.body
-
     /**
      * @note(storybook) applies attributes to iframe
      */
     htmlTag.setAttribute('class', theme || DEFAULT_THEME)
     htmlTag.setAttribute('data-mode', theme || DEFAULT_THEME)
     htmlTag.setAttribute('style', `color-scheme: ${theme || DEFAULT_THEME};`)
-    bodyTag.setAttribute('class', `radix-themes`)
   }, [theme])
 
   return (
