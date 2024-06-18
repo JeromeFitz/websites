2f25e1b650d4173f35af55ebad296274dfc11f12
Jerome Fitzgerald <j@jeromefitzgerald.com>
2024-06-17 20:20:05 -0400

    🩹  NICE-125 event info + skip music api [b] (#2558)
    Spotify API is kind of like ... not working. So this should be a direct Redis Cache hit in the menatime and something is really making it take forever. We know what the data is. Just set it and forget it until this needs to be dynamic again.

--- sites/jeromefitzgerald.com/src/app/playground/2024/_components/Currently.Music.Client.tsx
+++ sites/jeromefitzgerald.com/src/app/playground/2024/_components/Currently.Music.Client.tsx
@@ -1,34 +1,41 @@
 'use client'
-import { fetcher } from '@jeromefitz/shared/lib'
+/**
+ * @todo(api) NICE-125 this takes way too long to run
+ *                on what should be a redis cache hit
+ */
+// import { fetcher } from '@jeromefitz/shared/lib'
 
-import useSWR from 'swr'
+// import useSWR from 'swr'
 
-import { INIT, getKey } from '@/utils/getKey'
+// import { INIT, getKey } from '@/utils/getKey'
 
 import { CurrentlyItem } from './Currently.Item'
 import { CurrentlyWrapper } from './Currently.Item.Wrapper'
 
-const key = getKey(0, { ...INIT, time_range: 'long_term', type: 'top-tracks' })
-const options = {}
+// const key = getKey(0, { ...INIT, time_range: 'long_term', type: 'top-tracks' })
+// const options = {}
 
 function CurrentlyMusicClient({ titleSub, ...c }) {
   const { color, href, icon, id, title } = c
   const propsParent = { color, href, icon, id, title }
-  const { data, error, isLoading } = useSWR(key, fetcher, options)
+  // const { data, error, isLoading } = useSWR(key, fetcher, options)
 
-  const hasError = !!error
+  // const hasError = !!error
 
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore
-  const top = hasError ? {} : data?.items[0]
+  // const top = hasError ? {} : data?.items[0]
 
-  const headline = hasError ? titleSub[0] : top?.artist
-  const subline = hasError ? titleSub[1] : top?.album?.name
+  // const headline = hasError ? titleSub[0] : top?.artist
+  // const subline = hasError ? titleSub[1] : top?.album?.name
+
+  const headline = titleSub[0]
+  const subline = titleSub[1]
 
   const props = {
     headline,
     id,
-    isLoading,
+    isLoading: false,
     subline,
   }
 
