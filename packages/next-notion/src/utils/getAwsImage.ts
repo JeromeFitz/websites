faf39f8bf50f16dd942391b1a79b4c1223ea38a9
Jerome Fitzgerald <j@jeromefitzgerald.com>
2024-09-22 16:49:18 -0400

    ➕ (deps) @date-fns/tz|utc - refactor awsImage
    Verification:
    ```bash
    jeromefitzgerald.com:dev: 'utc:          2024-09-22T20:45:58.4558Z'
    jeromefitzgerald.com:dev: 'expiry_time:  2024-09-22T12:31:19.940Z'
    jeromefitzgerald.com:dev: 'diff:         29678 (494.6333333333333 minutes)'
    jeromefitzgerald.com:dev: 'isExpired:    y'
    jeromefitzgerald.com:dev: '---'
    jeromefitzgerald.com:dev: 'utc:          2024-09-22T20:45:58.4558Z'
    jeromefitzgerald.com:dev: 'expiry_time:  2024-09-22T12:31:19.940Z'
    jeromefitzgerald.com:dev: 'diff:         29678 (494.6333333333333 minutes)'
    jeromefitzgerald.com:dev: 'isExpired:    y'
    jeromefitzgerald.com:dev: '---'
    jeromefitzgerald.com:dev: 'SEO_IMAGE_IS_AWS: y'
    jeromefitzgerald.com:dev: 'utc:          2024-09-22T20:45:59.4559Z'
    jeromefitzgerald.com:dev: 'expiry_time:  2024-09-22T21:45:58.919Z'
    jeromefitzgerald.com:dev: 'diff:         -3599 (-59.983333333333334 minutes)'
    jeromefitzgerald.com:dev: 'isExpired:    n'
    jeromefitzgerald.com:dev: '---'
    ```

--- packages/next-notion/src/utils/getAwsImage.ts
+++ packages/next-notion/src/utils/getAwsImage.ts
@@ -1,6 +1,6 @@
 // import url from 'node:url'
-
-import { formatInTimeZone } from '@jeromefitz/date-fns-tz'
+import { TZDate } from '@date-fns/tz'
+import { format } from 'date-fns'
 
 import { differenceInSeconds, isAfter, parseISO } from 'date-fns'
 // import { isAfter, parseISO } from 'date-fns'
@@ -14,55 +14,6 @@ const URL_AWS = 'amazonaws.com'
 function isAwsImage(imageUrl: string) {
   return imageUrl?.includes(URL_AWS) || false
 }
-
-/**
- * @todo(deprecate) Lol, this is why you use the Notion API better
- *  so you can avoid doing hacks like this -- when there is an actual
- *  field that just tells you in `expiry_time` :X
- */
-// function isAwsImageExpired(imageUrl: string) {
-//   const timestamp = new Date()
-//   const tsAWS = formatInTimeZone(timestamp, TZ_AWS, `yyyyMMdd'T'HHmmss'Z'`)
-//   // const tsUTC = formatInTimeZone(timestamp, TZ_UTC, `yyyyMMdd'T'HHmmss'Z'`)
-//   const url_parts = url.parse(imageUrl, true)
-//   const query = url_parts.query
-//   const xAmzDate = query['X-Amz-Date']
-//   const xAmzExpires = query['X-Amz-Expires']
-//   // const dateLeft = parseISO(tsUTC)
-//   const dateLeft = parseISO(tsAWS)
-//   // @ts-ignore
-//   const dateRight = parseISO(xAmzDate)
-//   /**
-//    * @note(aws)
-//    * X-Amz-Date must not be more than X-Amz-Expires number of seconds in the past
-//    *
-//    */
-//   let diffInSeconds = differenceInSeconds(dateLeft, dateRight)
-//   diffInSeconds = diffInSeconds < 0 ? diffInSeconds * -1 : diffInSeconds
-//   // @ts-ignore
-//   const isExpired = diffInSeconds >= xAmzExpires
-
-//   // // @debug
-//   // if (DEBUG) {
-//   //   console.dir(`>> isAwsImageExpired`)
-//   //   console.dir(imageUrl)
-//   //   // console.dir(`ts(utc):           ${tsUTC}`)
-//   //   console.dir(`ts(aws):           ${tsAWS}`)
-//   //   console.dir(`xAmzDate:          ${xAmzDate}`)
-//   //   console.dir(`xAmzExpires:       ${xAmzExpires}`)
-//   //   // console.dir(`dateLeft:`)
-//   //   // console.dir(dateLeft)
-//   //   console.dir(`dateLeft:`)
-//   //   console.dir(dateLeft)
-//   //   console.dir(`dateRight:`)
-//   //   console.dir(dateRight)
-//   //   console.dir(`diffInSeconds:     ${diffInSeconds}`)
-//   //   console.dir(`isExpired:         ${isExpired ? 'y' : 'n'}`)
-//   //   console.dir(`---`)
-//   // }
-
-//   return isExpired
-// }
 
 /**
  * @note(notion)
@@ -81,8 +32,10 @@ function isImageExpired(image) {
   }
 
   const timestamp = new Date()
+  // @todo(types)
   // @note(notion) format in notion api (not aws query param)
-  const utc = formatInTimeZone(timestamp, TZ_UTC, `yyyy-MM-dd'T'HH:mm:ss.ms'Z'`)
+  let utc: any = new TZDate(timestamp, TZ_UTC)
+  utc = format(utc, `yyyy-MM-dd'T'HH:mm:ss.ms'Z'`)
   const isExpired = isAfter(parseISO(utc), parseISO(image?.expiry_time))
 
   // @debug
