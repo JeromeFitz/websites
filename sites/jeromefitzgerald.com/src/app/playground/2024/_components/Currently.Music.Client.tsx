0c731db897413dc89ca05321f33974658eecf426
Jerome Fitzgerald <j@jeromefitzgerald.com>
2025-01-17 22:33:30 -0500

    🐛 (music)  NICE-133 recently_played [b] (#3160)
    Haha, whoops. I had the wrong `SPOTIFY_REFRESH_TOKEN`. That is updated in all `.env` et al.
    
    Eh, using Tidal more anyway and going away from Spotify. But until then finish up the `recently-played` route.

--- sites/jeromefitzgerald.com/src/app/playground/2024/_components/Currently.Music.Client.tsx
+++ sites/jeromefitzgerald.com/src/app/playground/2024/_components/Currently.Music.Client.tsx
@@ -3,39 +3,42 @@
  * @todo(api) NICE-125 this takes way too long to run
  *                on what should be a redis cache hit
  */
-// import { fetcher } from '@jeromefitz/shared/lib'
+import { fetcher } from '@jeromefitz/shared/lib'
 
-// import useSWR from 'swr'
+import useSWR from 'swr'
 
-// import { INIT, getKey } from '@/utils/getKey'
+import { getKey, INIT } from '@/utils/getKey'
 
 import { CurrentlyItem } from './Currently.Item'
 import { CurrentlyWrapper } from './Currently.Item.Wrapper'
 
-// const key = getKey(0, { ...INIT, time_range: 'long_term', type: 'top-tracks' })
-// const options = {}
+// const key = getKey(0, { ...INIT, time_range: 'short_term', type: 'top-tracks' })
+const key = getKey(0, { ...INIT, limit: 50, type: 'recently-played' })
+
+const options = {}
 
 function CurrentlyMusicClient({ titleSub, ...c }) {
   const { color, href, icon, id, title } = c
   const propsParent = { color, href, icon, id, title }
-  // const { data, error, isLoading } = useSWR(key, fetcher, options)
+  const { data, error, isLoading } = useSWR(key, fetcher, options)
 
-  // const hasError = !!error
+  const hasError = !!error
 
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore
-  // const top = hasError ? {} : data?.items[0]
+  const top = hasError ? {} : data?.items[0]
 
-  // const headline = hasError ? titleSub[0] : top?.artist
+  const headline = hasError ? titleSub[0] : top?.artist
   // const subline = hasError ? titleSub[1] : top?.album?.name
+  const subline = hasError ? titleSub[1] : top?.name
 
-  const headline = titleSub[0]
-  const subline = titleSub[1]
+  // const headline = titleSub[0]
+  // const subline = titleSub[1]
 
   const props = {
     headline,
     id,
-    isLoading: false,
+    isLoading,
     subline,
   }
 
