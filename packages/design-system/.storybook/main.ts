cb6355352a5ae6fe558dff9a17486f349e00036f
Jerome Fitzgerald <j@jeromefitzgerald.com>
2025-08-03 10:55:49 -0400

    ♻️  finish migration (!eslint)

--- packages/design-system/.storybook/main.ts
+++ packages/design-system/.storybook/main.ts
@@ -1,1 +1,2 @@
 export { default } from '@jeromefitz/storybook-config/main.config'
+export const addons = ['@storybook/addon-docs'];
--- packages/storybook-config/main.config.ts
+++ packages/storybook-config/main.config.ts
@@ -7,26 +7,8 @@ import { resolve } from 'node:path'
 const config = {
   addons: [
     '@storybook/addon-links',
-    {
-      name: '@storybook/addon-essentials',
-      options: {
-        actions: true,
-        backgrounds: false,
-        controls: true,
-        docs: true,
-        toolbars: true,
-        viewport: true,
-      },
-    },
-    '@storybook/addon-interactions',
     '@storybook/addon-a11y',
-    // '@storybook/addon-styling',
-    {
-      name: '@storybook/addon-styling',
-      options: {
-        postCss: true,
-      },
-    },
+    '@storybook/addon-themes',
   ],
   core: {
     disableTelemetry: true,
